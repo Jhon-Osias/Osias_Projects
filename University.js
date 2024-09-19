465f330a69f1e2b1df1b0b0cb718072866cde38e
Jhon-Osias <jhonvaldwin@gmail.com>
2024-09-19 18:14:49 +0800

    Update University.js

--- University.js
+++ University.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/coreValues', (req, res) => {
+    res.render('coreValues', {
+        title: 'City of Malabon University Core Values'
+    });
+});
+
+module.exports = router;
+
 
